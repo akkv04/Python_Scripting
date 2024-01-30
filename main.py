8b0c8953c3e3dbab61279bdc4a2e834a19e3dcac
EC2 Default User <ec2-user@ip-172-31-43-43.ap-southeast-2.compute.internal>
2024-01-30 02:17:54 +0000

    updating main.py

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ from collections import defaultdict
 from operator import itemgetter
 
 
-def download_architecture_contents_file(input_architecture):
-    # Hard coding the debian mirror url as provided, if the mirror url has to be changed,
-    # below variable has to be modified accordingly
-    debian_mirror_url = "http://ftp.uk.debian.org/debian/dists/stable/main/"
-    architecture_contents_url = f"{debian_mirror_url}/Contents-{input_architecture}.gz"
+def download_architecture_contents_file(input_arch):
+    # Hard coding the debian mirror url as provided
+    # Please modify according to the need
+    mirror_url = "http://ftp.uk.debian.org/debian/dists/stable/main/"
+    architecture_contents_url = f"{mirror_url}/Contents-{input_arch}.gz"
 
     try:
         # Raises an HTTPError for bad responses (e.g., 404, 500)
         response = requests.get(architecture_contents_url, stream=True)
         response.raise_for_status()
 
-        total_size = int(response.headers.get('content-length', 0))
+        total_size = int(response.headers.get("content-length", 0))
 
         # Setting up tqdm to display the download progress bar
-        tqdm_download_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+        tqdm_download_bar = tqdm(total=total_size, unit="B", unit_scale=True)
 
         # iterates over the response content in chunks of 8192 bytes
-        with open(f"Contents-{input_architecture}.gz", "wb") as f:
+        with open(f"Contents-{input_arch}.gz", "wb") as f:
             for chunk in response.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
@@ -49,19 +49,23 @@ def extract_architecture_contents_file():
         contents_file_name = contents_file_path.replace(".gz", "")
 
         # extracting the content and saving to a new file
-        with gzip.open(contents_file_path, 'rb') as f_in, open(contents_file_name, 'wb') as f_out:
+        with gzip.open(contents_file_path, "rb") as f_in, open(
+            contents_file_name, "wb"
+        ) as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-        print(f"Extracted {contents_file_path} to {contents_file_name} successfully")
+        print(
+            f"Extracted {contents_file_path} to {contents_file_name} successfully")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
 
-def parse_architecture_contents_file(file_path):
+def parse_arch_contents_file(file_path):
     package_files_count_check = defaultdict(int)
 
     try:
-        # parsing the contents of a gzipped file line by line and counting occurrences of package names
+        # parsing the contents of a gzipped file line by line and
+        # counting occurrences of package names
         with gzip.open(file_path, "rt", encoding="utf-8", errors="ignore") as f:
             for line in f:
                 parts = line.strip().split(" ")
@@ -76,7 +80,9 @@ def parse_architecture_contents_file(file_path):
 
 
 def display_architecture_top_packages(package_files_count_check):
-    sorted_packages = sorted(package_files_count_check.items(), key=itemgetter(1), reverse=True)
+    sorted_packages = sorted(
+        package_files_count_check.items(), key=itemgetter(1), reverse=True
+    )
 
     print("\nTop 10 Packages:")
     for i, (package, count) in enumerate(sorted_packages[:10], start=1):
@@ -91,7 +97,7 @@ if __name__ == "__main__":
     print(f"Downloading the Contents-{architecture}.gz\n")
     print("=====================================")
     if download_architecture_contents_file(architecture):
-        # if the content file has been downloaded, function will return True and this block will be executed
+        # if the content file has been downloaded, function will return True
         print(f"\n Extracting the contents of Contents-{architecture}.gz")
 
         contents_file_path = f"Contents-{architecture}.gz"
@@ -99,11 +105,10 @@ if __name__ == "__main__":
         print("=====================================")
 
         print("\nCounting the files and resp packages")
-        package_files_count = parse_architecture_contents_file(contents_file_path)
+        package_files_count = parse_arch_contents_file(contents_file_path)
         print("\n")
         print("=====================================")
         display_architecture_top_packages(package_files_count)
         print("=====================================")
         print("Execution Finished\n")
-        print("Refactoring Python")
-        
+        print("Refactoring Python-1")
