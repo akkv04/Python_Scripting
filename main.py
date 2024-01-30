8d63f74431746ac552bdc87f49766d25b218a3ee
Akhil K V <64690489+akkv04@users.noreply.github.com>
2024-01-30 06:44:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,31 +3,33 @@ import sys
 import requests
 import tarfile
 from tqdm import tqdm
-from collections import defaultdict
-from operator import itemgetter
 import gzip
 import shutil
+from collections import defaultdict
+from operator import itemgetter
 
-def download_contents_file(architecture):
-    mirror_url = "http://ftp.uk.debian.org/debian/dists/stable/main/"
-    contents_url = f"{mirror_url}/Contents-{architecture}.gz"
-    print("before download")
+def download_architecture_contents_file(architecture):
+    #Hardcoding the debian mirror url as provided, if the mirror url has to be changed, below variable has to be modified accordingly
+    debian_mirror_url = "http://ftp.uk.debian.org/debian/dists/stable/main/"
+    architecture_contents_url = f"{debian_mirror_url}/Contents-{architecture}.gz"
+   
     try:
-        response = requests.get(contents_url, stream=True)
+        # Raises an HTTPError for bad responses (e.g., 404, 500)
+        response = requests.get(architecture_contents_url, stream=True)
         response.raise_for_status()
 
         total_size = int(response.headers.get('content-length', 0))
 
         # Setting up tqdm to display the progress bar
-        tqdm_bar = tqdm(total=total_size, unit='B', unit_scale=True)
+        tqdm_download_bar = tqdm(total=total_size, unit='B', unit_scale=True)
 
         with open(f"Contents-{architecture}.gz", "wb") as f:
             for chunk in response.iter_content(chunk_size=8192):
                 if chunk:
                     f.write(chunk)
-                    tqdm_bar.update(len(chunk))
+                    tqdm_download_bar.update(len(chunk))
 
-        tqdm_bar.close()
+        tqdm_download_bar.close()
 
         return True
     except requests.exceptions.RequestException as e:
@@ -35,27 +37,25 @@ def download_contents_file(architecture):
         return False
 
         
-def extract_contents_file(architecture):
-    gz_file_path = f"Contents-{architecture}.gz"
-    print(gz_file_path)
-    print(os.getcwd())
-    
+def extract_architecture_contents_file(architecture):
+   
+   
     try:
-        if not os.path.exists(gz_file_path):
-            print(f"Error: File not found: {gz_file_path}")
+        if not os.path.exists(contents_file_path):
+            print(f"Error: File not found: {contents_file_path}")
             return
 
         # Create a new file with the same name without the ".gz" extension
-        tar_file_path = gz_file_path.replace(".gz", "")
+        contents_file_name = contents_file_path.replace(".gz", "")
 
-        with gzip.open(gz_file_path, 'rb') as f_in, open(tar_file_path, 'wb') as f_out:
+        with gzip.open(contents_file_path, 'rb') as f_in, open(contents_file_name, 'wb') as f_out:
             shutil.copyfileobj(f_in, f_out)
 
-        print(f"Successfully decompressed {gz_file_path} to {tar_file_path}")
+        print(f"Successfully decompressed {contents_file_path} to {contents_file_name}")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-def parse_contents_file(file_path):
+def parse_architecture_contents_file(file_path):
     package_files_count = defaultdict(int)
 
     try:
@@ -71,7 +71,7 @@ def parse_contents_file(file_path):
 
     return package_files_count
 
-def display_top_packages(package_files_count):
+def display_architecture_top_packages(package_files_count):
     sorted_packages = sorted(package_files_count.items(), key=itemgetter(1), reverse=True)
 
     print("\nTop 10 Packages:")
@@ -84,12 +84,18 @@ if __name__ == "__main__":
     architecture = sys.argv[1]
     print("=====================================")
     print(f"Downloading the Contents-{architecture}.gz\n")
-    if download_contents_file(architecture):
+    print("=====================================")
+    if download_architecture_contents_file(architecture):
         #if the content file has been downloaded, function will return True and this block will be executed
         print(f"\n Extracting the contents of Contents-{architecture}.gz")
-        extract_contents_file(architecture)
-        gz_file_path = f"Contents-{architecture}.gz"
+        
+        contents_file_path = f"Contents-{architecture}.gz"
+        extract_architecture_contents_file(architecture)
+        print("=====================================")
+        
         print("\nCounting the files and resp packages")        
-        package_files_count = parse_contents_file(gz_file_path)
+        package_files_count = parse_architecture_contents_file(contents_file_path)
         print("\n")
-        display_top_packages(package_files_count)
+        print("=====================================")
+        display_architecture_top_packages(package_files_count)
+        print("=====================================")
